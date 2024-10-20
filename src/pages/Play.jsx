c2b6a3f8dd8e2c00d3543206331858f8f6f57022
Manoj99Q <gmanojcs9@gmail.com>
2024-10-20 17:45:28 -0500

    feat: Add Map3DAdvanced component to Play page

--- src/pages/Play.jsx
+++ src/pages/Play.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Map3D from '../components/Map3D';
 import Map3DModelTest from '../components/Map3DModelTest';
+import Map3DAdvanced from '../components/Map3DAdvanced';
 
 const Play = () => {
     return (
@@ -16,9 +17,11 @@ const Play = () => {
                     tilt="67.5"
                 />
             </div> */}
-            <div className="w-3/4 bg-gray-400 h-full flex-grow">
+            {/* <div className="w-3/4 bg-gray-400 h-full flex-grow">
                 <Map3DModelTest />
-            </div>
+            </div> */}
+
+                <Map3DAdvanced />
         </div>
     );
 };
