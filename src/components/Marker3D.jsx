3d51011ed61e2209b1c3f0cf518686b12d9e2b66
Manoj99Q <gmanojcs9@gmail.com>
2024-10-20 21:57:38 -0500

    chore: Remove commented-out altitudeMode code in Marker3D component

--- src/components/Marker3D.jsx
+++ src/components/Marker3D.jsx
@@ -22,7 +22,7 @@ const Marker3D = ({ marker }) => {
 
         const markerOptions = {
           position: marker.position,
-          altitudeMode: marker.altitudeMode ?? (google.maps.AltitudeMode ? google.maps.AltitudeMode.ABSOLUTE : "absolute"),
+          // altitudeMode: marker.altitudeMode ?? (google.maps.AltitudeMode ? google.maps.AltitudeMode.ABSOLUTE : "absolute"),
           collisionBehavior: marker.collisionBehavior || "required",
           drawsWhenOccluded: marker.drawsWhenOccluded || false,
           extruded: marker.extruded || false,
